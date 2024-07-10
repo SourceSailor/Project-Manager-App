81ef67da28126323b83e62964d49a639d5941e21
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2024-07-09 17:34:00 -0700

    Creation of ProjectSidebar Component

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProjectsSidebar = () => {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button type="button">+ Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+};
+
+export default ProjectsSidebar;
